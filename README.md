2cd757c00e65bca0eba2fe3873801646f182ff55
Carlos Sánchez <holapueblodev@gmail.com>
2023-01-19 11:50:53 +0100

    Practicando con API

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # e-comerce
+<div align="center">
+  <img align="center" alt="HTML" title="HTML 5" height="50" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+  <img align="center" alt="CSS" title="CSS 3" height="50" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+  <img align="center" alt="JavaScript" title="JavaScript" height="50" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+</div>
--- js/API.respond.js
+++ js/API.respond.js
@@ -1,23 +1,10 @@
 
-
 fetch('https://api.chatbot.com/v2/stories/63c7ed581c9d010007d555b8', {
   headers: {
     'authorization': 'Bearer '
   }
   
 })
-  .then(response => response.json())
+  .then(response => console.info(response))
   .then(data => console.log(data))
   .catch(error => console.error(error))
-
-  
-
-
-  
-//fetch('https://api.chatbot.com/v2/stories/63c7ed581c9d010007d555b8')
-  
-
-// curl --request GET \
-// 	--url https://api.chatbot.com/v2/stories/63c7ed581c9d010007d555b8 \
-// 	--header 'authorization: Bearer ${HUtQMf76t_UMbjsWRi4NKLkbLOFT_C5K}'
-
